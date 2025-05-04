1261ed1da442c6465863c38ec8c2442962155f03
pavan-pani <pavanpk1012@gmail.com>
2025-05-04 12:10:49 +0530

    counter app

--- src/Coding/Counter.jsx
+++ src/Coding/Counter.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react"
+
 const Counter =()=>{
+    const [count, setCount] = useState(0)
     return(
         <>
-            <p>This counter</p>
+            <p>This counter is {count}</p>
+            <button onClick={()=>setCount(prev => count >= 10 ? 0 : prev +1)}> + </button>
+            <button onClick={()=>setCount(prev => count <=0 ? 0 : prev -1)}> - </button>
+            <button onClick={()=>setCount(0)}> reset </button>
         </>
     )
 }
